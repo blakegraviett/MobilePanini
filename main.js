19ceb4bd40bc3609ccc83e6ca7eb9bbaac0803ee
Blake <blake.graviett@gmail.com>
2023-02-23 10:27:19 -0600

    fix theme switcher

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ const normalButton = document.getElementById('normal');
 const body = document.body;
 
 darkButton.onclick = () => {
-    body.classList.replace('normal', 'dark');
-    body.classList.replace('light', 'dark');
+    body.classList.remove('normal');
+    body.classList.remove('light');
+    body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
 }
-
 normalButton.onclick = () => {
-    body.classList.replace('dark', 'normal');
-    body.classList.replace('light', 'normal');
+    body.classList.remove('dark');
+    body.classList.remove('light');
+    body.classList.add('normal');
+    localStorage.setItem('theme', 'normal');
 }
 lightButton.onclick = () => {
-    body.classList.replace('normal', 'light');
-    body.classList.replace('dark', 'light');
+    body.classList.remove('dark');
+    body.classList.remove('normal');
+    body.classList.add('light');
+   localStorage.setItem('theme', 'light');
+}
+
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+    body.classList.add(theme);
 }
